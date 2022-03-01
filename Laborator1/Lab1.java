8f4ff3df5a1ca935266c4505337de45b9b8c617f
Berea Manuela-Mihaela <77302093+ManuBerea@users.noreply.github.com>
2022-03-01 18:18:39 +0200

    Update Lab1.java

--- Laborator1/Lab1.java
+++ Laborator1/Lab1.java
@@ -1,9 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Lab1 {
     public static void main(String[] args) {
         Lab1 lab1 = new Lab1();
+
+        System.out.println("Compulsory:");
         lab1.compulsory();
-        lab1.homework();
-        lab1.bonus();
+        System.out.println();
+
+        // 4. For larger n display the running time of the application in nanoseconds (DO NOT display the data structure!).
+        long start = System.nanoTime();
+        String[] input = {"7","3","q","w","e","r","t","y"};
+        System.out.println("Homework:");
+        lab1.homework(input);
+        long end = System.nanoTime();
+        long time = end - start;
+        System.out.println("The running time of the application in nanoseconds is: " + time);
     }
 
     static int sumOfDigits(int number) {
@@ -30,12 +44,100 @@ public class Lab1 {
         System.out.println("Willy-nilly, this semester I will learn " + languages[result]);
     }
 
-    void homework() {
-        // TO DO
-    }
+    void homework(String[] args) {
+        // 1. Let n, p be two integers and C1,...,Cm a set of letters (the alphabet), all given as a command line arguments. Validate the arguments!
+        boolean firstInt = Character.isDigit(args[0].charAt(0));
+        boolean secondInt = Character.isDigit(args[1].charAt(0));
 
-    void bonus() {
-        // TO DO
+        if (args.length <= 2) {
+            System.out.println("The input has not enough arguments!");
+            return;
+        }
+        if (!firstInt) {
+            System.out.println("The input for the variable n must be an integer!");
+            return;
+        }
+        if (!secondInt) {
+            System.out.println("The input for the variable p must be an integer!");
+            return;
+        }
+        String[] alphabet = Arrays.copyOfRange(args, 2, args.length);
+        for (String s : alphabet) {
+            if (Character.isDigit(s.charAt(0))) {
+                System.out.println("The input for the alphabet contains integers!");
+                return;
+            }
+        }
+
+        int n, p;
+        ArrayList<String> letters = new ArrayList<>();
+        n = Integer.parseInt(args[0]);
+        p = Integer.parseInt(args[1]);
+        System.out.println("Integers: " + n + " " + p);
+        System.out.print("Letters: ");
+        for (String s : alphabet) {
+            letters.add(s);
+            System.out.print(s + " ");
+        }
+        System.out.println();
+
+        // 2. Create an array of n strings (called words), each word containing exactly p characters from the given alphabet. Display on the screen the generated array.
+
+        ArrayList<StringBuilder> words = new ArrayList<>();
+        System.out.print("Random words generated: ");
+        for (int i = 0; i < n; i++) {
+            Random random = new Random();
+            StringBuilder randomWord = new StringBuilder();
+            for (int j = 0; j < p; j++) {
+                int randomLetter = random.nextInt(letters.size());
+                randomWord.append(letters.get(randomLetter));
+            }
+            words.add(randomWord);
+            System.out.print(words.get(i) + " ");
+        }
+        System.out.println();
+
+    /* 3. Two words are neighbors if they have a common letter.
+        - Create a boolean n x n matrix, representing the adjacency relation of the words.
+        - Create a data structure (using arrays) that stores the neighbors of each word. Display this data structure on the screen. */
+
+        boolean[][] adjacencyMatrix = new boolean[n][n];
+        String[] neighbors = new String[n];
+        for (int i = 0; i < n; i++) {
+            neighbors[i] = "";
+        }
+
+        for (int i = 0; i < n; i++) {
+            StringBuilder currentWord = words.get(i);
+            for (int j = 0; j < n; j++) {
+                StringBuilder anotherWord = words.get(j);
+                boolean isNeighbor = false;
+                    for(int x = 0; x < currentWord.length(); x++) {
+                        for(int y = 0; y < anotherWord.length(); y++) {
+                            if (currentWord.charAt(x) == anotherWord.charAt(y)) {
+                                isNeighbor = true;
+                                break;
+                            }
+                        }
+                        if(isNeighbor) {
+                            break;
+                        }
+                    }
+                adjacencyMatrix[i][j] = isNeighbor;
+
+               if(isNeighbor && i != j) {
+                   neighbors[i] = neighbors[i].concat(String.valueOf(words.get(j)));
+                   neighbors[i] = neighbors[i].concat(" ");
+               }
+             //   System.out.print(adjacencyMatrix[i][j] + " "); // to print the boolean matrix on the screen
+            }
+          //  System.out.println();
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Neighbors of " + words.get(i) + " are: ");
+            System.out.print(neighbors[i] + " ");
+            System.out.println();
+        }
     }
 }
-
