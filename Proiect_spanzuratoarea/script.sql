91e41a29375400bb38a0abfac06928d0cfee7663
Berea Manuela-Mihaela <77302093+ManuBerea@users.noreply.github.com>
2022-06-04 15:52:29 +0300

    Delete Proiect_spanzuratoarea directory

--- Proiect_spanzuratoarea/script.sql
+++
@@ -1,9 +0,0 @@
-drop table words;
-
-CREATE TABLE words (
-  id INT NOT NULL PRIMARY KEY,
-  word_name VARCHAR2(30) NOT NULL,
-  word_language VARCHAR2(30) NOT NULL,
-  word_difficulty VARCHAR2(30) NOT NULL
-)
-/
